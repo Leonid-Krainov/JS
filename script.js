bbbac49d954c844f805b41ee858ca218f01c93b3
Leonid-Krainov <KrajnovLeonid@gmail.com>
2017-08-29 23:31:44 +0300

    Done task

---
+++ script.js
@@ -0,0 +1,16 @@
+function runOnButton1() {
+    var name = "Leonid";
+    var time = "21:30";
+    var place = "home";
+    var event = prompt("Enter event","party");
+    var result = Name + " invites all at " + Time + " in the " + Place + " for the " + event;
+    console.log(result);
+}
+function runOnButton2() {
+    var euro = prompt("Enter sum of euro","0")
+    var grnEu = euro * 30.65
+    var dollar = prompt("Enter sum of dollars","0")
+    var grnDol = dollar * 25.44
+    var calculation = euro + " euros are equal " + grnEu.toFixed(2) + " grns, " + dollar + " dollars are equal " + grnDol.toFixed(2) + " grns, 1 euro is equal " + (30.65/25.44).toFixed(2) + " dollars"
+    alert(calculation)
+}
